9713318dcfdfb74fe6e822dcba3188d86eb98790
Kristen Chandler <krismchandler@gmail.com>
2022-07-21 21:08:51 -0500

    started new plyer form for create

--- pages/new.js
+++ pages/new.js
@@ -1,7 +1,125 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+import { Button } from 'react-bootstrap';
+import { useAuth } from '../utils/context/authContext';
+import { createPlayer, getPlayers, updatePlayer } from '../api/playerData';
 
-export default function addPlayer() {
+const initialState = {
+  description: '',
+  image: '',
+  price: '',
+  sale: false,
+  title: '',
+};
+
+function PlayerForm({ obj }) {
+  const [formInput, setFormInput] = useState(initialState);
+  const [players, setPlayers] = useState([]);
+  const router = useRouter();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    getPlayers(user.uid).then(setPlayers);
+
+    if (obj.firebaseKey) setFormInput(obj);
+  }, [obj, user]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (obj.firebaseKey) {
+      updatePlayer(formInput)
+        .then(() => router.push(`/players/${obj.firebaseKey}`));
+    } else {
+      const payload = { ...formInput, uid: user.uid };
+      createPlayer(payload).then(() => {
+        router.push('/');
+      });
+    }
+  };
+
   return (
-    <h1>Add A Player</h1>
+    <Form onSubmit={handleSubmit}>
+      <h2 className="text-white mt-5">{obj.firebaseKey ? 'Update' : 'Create'} Book</h2>
+      <FloatingLabel controlId="floatingInput1" label="Book Title" className="mb-3">
+        <Form.Control type="text" placeholder="Enter a title" name="title" value={formInput.title} onChange={handleChange} required />
+      </FloatingLabel>
+      <FloatingLabel controlId="floatingInput2" label="Book Image" className="mb-3">
+        <Form.Control type="url" placeholder="Enter an image url" name="image" value={formInput.image} onChange={handleChange} required />
+      </FloatingLabel>
+      <FloatingLabel controlId="floatingInput3" label="Book Price" className="mb-3">
+        <Form.Control type="text" placeholder="Enter price" name="price" value={formInput.price} onChange={handleChange} required />
+      </FloatingLabel>
+
+      <FloatingLabel controlId="floatingSelect" label="Author">
+        <Form.Select
+          aria-label="Author"
+          name="author_id"
+          onChange={handleChange}
+          className="mb-3"
+          required
+        >
+          <option value="">Select an Author</option>
+          {
+            players.map((player) => (
+              <option
+                key={player.firebaseKey}
+                value={player.firebaseKey}
+                selected={obj.author_id === player.firebaseKey}
+              >
+                {player.first_name} {player.last_name}
+              </option>
+            ))
+          }
+        </Form.Select>
+      </FloatingLabel>
+
+      <FloatingLabel controlId="floatingTextarea" label="Description" className="mb-3">
+        <Form.Control as="textarea" placeholder="Description" style={{ height: '100px' }} name="description" value={formInput.description} onChange={handleChange} required />
+      </FloatingLabel>
+
+      {/* A WAY TO HANDLE UPDATES FOR TOGGLES, RADIOS, ETC  */}
+      <Form.Check
+        className="text-white mb-3"
+        type="switch"
+        id="sale"
+        name="sale"
+        label="On Sale?"
+        checked={formInput.sale}
+        onChange={(e) => setFormInput((prevState) => ({
+          ...prevState,
+          sale: e.target.checked,
+        }))}
+      />
+      <Button type="submit">{obj.firebaseKey ? 'Update' : 'Create'} Book</Button>
+    </Form>
   );
 }
+
+PlayerForm.propTypes = {
+  obj: PropTypes.shape({
+    description: PropTypes.string,
+    image: PropTypes.string,
+    price: PropTypes.string,
+    sale: PropTypes.bool,
+    title: PropTypes.string,
+    author_id: PropTypes.string,
+    firebaseKey: PropTypes.string,
+  }),
+};
+
+PlayerForm.defaultProps = {
+  obj: initialState,
+};
+
+export default PlayerForm;
