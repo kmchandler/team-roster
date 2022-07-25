b60ffc27444700d486f395e354951509ea60690d
Kristen Chandler <krismchandler@gmail.com>
2022-07-24 21:28:23 -0500

    styled login page as a card

--- components/Signin.js
+++ components/Signin.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
 import { signIn } from '../utils/auth';
 
 function Signin() {
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        margin: '0 auto',
-      }}
-    >
-      <div>
-        <h1 className="roost">ROOST</h1>
-        <h4 className="taglineLogin">your home for team management</h4>
-      </div>
-      <Button type="button" size="lg" className="copy-btn loginBtn" onClick={signIn}>
-        Sign In
-      </Button>
-    </div>
+    <Card className="loginCardDiv">
+      <Card.Body>
+        <Card.Title className="roost">ROOST</Card.Title>
+        <Card.Subtitle className="taglineLogin">your home for team management</Card.Subtitle>
+        <Button type="button" size="lg" className="copy-btn loginBtn" onClick={signIn}>
+          Sign In
+        </Button>
+      </Card.Body>
+    </Card>
   );
 }
 
