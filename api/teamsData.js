a3327c2091af01e4f203cfff31ec7e407e0d09eb
Kristen Chandler <krismchandler@gmail.com>
2022-07-28 10:01:19 -0500

    created teamsData and mergedData api calls

---
+++ api/teamsData.js
@@ -0,0 +1,65 @@
+import axios from 'axios';
+import firebaseConfig from './apiKeys';
+
+const dbUrl = firebaseConfig.databaseURL;
+
+const getTeams = (uid) => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/teams.json?orderBy="uid"&equalTo="${uid}"`)
+    .then((response) => {
+      if (response.data) {
+        resolve(Object.values(response.data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch((error) => reject(error));
+});
+
+const createTeam = (teamObj) => new Promise((resolve, reject) => {
+  axios.post(`${dbUrl}/teams.json`, teamObj)
+    .then((response) => {
+      const payload = { firebaseKey: response.data.name };
+      axios.patch(`${dbUrl}/teams/${response.data.name}.json`, payload)
+        .then(() => {
+          getTeams(teamObj.uid).then(resolve);
+        });
+    })
+    .catch(reject);
+});
+
+const getSingleTeam = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/teams/${firebaseKey}.json`)
+    .then((response) => resolve(response.data))
+    .catch((error) => reject(error));
+});
+
+const deleteSingleTeam = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.delete(`${dbUrl}/teams/${firebaseKey}.json`)
+    .then(() => {
+      getTeams(firebaseKey.uid).then(resolve);
+    })
+    .catch(reject);
+});
+
+const updateTeam = (teamObj) => new Promise((resolve, reject) => {
+  axios.patch(`${dbUrl}/teams/${teamObj.firebaseKey}.json`, teamObj)
+    .then(() => {
+      getTeams(teamObj.uid).then(resolve);
+    })
+    .catch(reject);
+});
+
+const getTeamPlayers = (firebaseKey) => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/players.json?orderBy="playerId"&equalTo="${firebaseKey}"`)
+    .then((response) => resolve(Object.values(response.data)))
+    .catch((error) => reject(error));
+});
+
+export {
+  getTeams,
+  createTeam,
+  getSingleTeam,
+  deleteSingleTeam,
+  updateTeam,
+  getTeamPlayers,
+};
